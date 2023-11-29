583cfa85ef8d34e688b4bfd5f85eebf9a4d79fd6
lukaszgajewski <lukaszgajewski@tuta.io>
2023-11-29 14:25:07 +0100

    add europe voronoi plot

--- plotVoronoiFromPkl.py
+++ plotVoronoiFromPkl.py
@@ -5,31 +5,33 @@ import pandas as pd
 import mapclassify as mc
 
 
-def plotCombinedVoronoi(sRegions, rate, cutoff, continent=None):
-    fig, ax = plt.subplots(figsize=(12, 10))
+def plotCombinedVoronoi(sRegions, region, rate):
+    _, ax = plt.subplots(figsize=(12, 10))
 
     sRegions["powerOut" + str(rate)] = sRegions[str(rate)] > 0.33
-
-    if continent == "Europe":
-        world = geopandas.read_file(geopandas.datasets.get_path("naturalearth_lowres"))
-        world = world[world["name"] == "Russia"]
-        inter = geopandas.sjoin(world, sRegions)
-        sRegions = sRegions[~sRegions.index.isin(inter["index_right"])]
 
     gplt.choropleth(
         sRegions,
         hue=sRegions["powerOut" + str(rate)],
         cmap="viridis",
         ax=ax,
-        legend=True,
-        legend_labels=["", "outage"],
+        legend=False,
+        # legend_labels=["", "outage"],
         edgecolor="None",
         scheme=mc.UserDefined(sRegions["powerOut" + str(rate)].values, [0, 1]),
     )
-    plt.title("Substation at Risk of Electricity Loss, " + str(rate) + "per Year Storm")
+    plt.title(
+        "Substation at Risk of Electricity Loss, " + str(rate) + " per Year Storm"
+    )
+    plt.savefig(
+        "Data/SmallData/Figures/Europe_USA/" + region + str(rate) + "peryearOutage.png",
+        bbox_inches="tight",
+    )
     plt.show()
-    # plt.savefig(Params.figuresDir + str(rate) + "peryearOutage.png")
 
 
-sRegions = pd.read_pickle("Data/SmallData/combinedVoronoi_europe_0.0001.pkl")
-plotCombinedVoronoi(sRegions, 0.0001, True, "Europe")
+file = "Data/SmallData/combinedVoronoi_europe_0.0001.pkl"
+sRegions = pd.read_pickle(file)
+rate = float(file[file.rfind("_") + 1 : file.rfind(".")])
+region = file[file.find("_") + 1 : file.rfind("_")]
+plotCombinedVoronoi(sRegions, region, rate)
